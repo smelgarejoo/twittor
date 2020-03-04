13b09d3495ac4b43fd6215b8f8e73ddb16e59c01
SERGIO MELGAREJO BERRIO <smelgarejo@LUCKY.local>
2020-03-04 12:28:13 -0500

    fc

---
+++ js/sw-utils.js
@@ -0,0 +1,22 @@
+function ActualizarCacheDinamico(dynamicCache, req, res) {
+
+
+
+    if (res.ok) {
+        return caches.open(dynamicCache).then(cache => {
+
+            cache.put(req, res.clone());
+
+            return res.clone();
+
+        });
+
+
+    } else {
+
+
+
+        return res;
+    }
+
+}
